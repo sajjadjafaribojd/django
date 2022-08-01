f59b880a3fce891095f48db91cba807677b35c6b
jabj <sajjadjafaribojd@gmail.com>
2022-08-01 18:57:44 +0000

    Add jabj app to settings.py

--- myworld/myworld/settings.py
+++ myworld/myworld/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'members.apps.MembersConfig'
+    'members.apps.MembersConfig',
+    'jabj.apps.JabjConfig',
 ]
 
 MIDDLEWARE = [
